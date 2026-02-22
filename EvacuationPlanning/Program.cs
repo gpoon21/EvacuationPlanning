8f0d19e31883b6043785d3a906a6f53b2f52959e
pipoon21 <pipoon22@hotmail.com>
2026-02-22 20:10:26 +0700

    Integrate Redis cache into service configuration.

--- EvacuationPlanning/Program.cs
+++ EvacuationPlanning/Program.cs
@@ -5,6 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddStackExchangeRedisCache(options => {
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
 
 var app = builder.Build();
 
