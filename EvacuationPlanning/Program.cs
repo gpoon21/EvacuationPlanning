f6172aab5bafa94e12f8ece1728701ee87031c29
pipoon21 <pipoon22@hotmail.com>
2026-02-24 15:30:07 +0700

    Refactor Program.cs for explicit variable declarations to improve code clarity and consistency.

--- EvacuationPlanning/Program.cs
+++ EvacuationPlanning/Program.cs
@@ -1,7 +1,7 @@
 using EvacuationPlanning;
 using EvacuationPlanning.Strategies;
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddSingleton<IStrategy, GeneticStrategy>();
@@ -14,7 +14,7 @@ builder.Services.AddStackExchangeRedisCache(options => {
     options.Configuration = builder.Configuration.GetConnectionString("Redis");
 });
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
